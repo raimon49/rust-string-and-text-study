7c9adc2d04c9c81fe52032617403606a6d56f932
raimon <raimon49@hotmail.com>
2021-07-23 17:40:58 +0900

    Add a code about assert 'udon' code point

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,12 @@ fn main() {
         // UnicodeはLatin-1のスーパーセットのため、相互の変換には変換テーブル不要
         assert_eq!(latin1_to_char(42), '*');
         assert_eq!(char_to_latin1('*'), Some(42));
+
+        assert_eq!("うどんudon".as_bytes(),
+                   &[0xe3, 0x81, 0x86, // う
+                     0xe3, 0x81, 0xa9, // ど
+                     0xe3, 0x82, 0x93, // ん
+                     0x75, 0x64, 0x6f, 0x6e //:udon
+                   ]);
     }
 }
