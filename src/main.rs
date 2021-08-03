a99b44562c5741e450a9bf7bf2eec2e6d4628100
raimon <raimon49@hotmail.com>
2021-08-03 18:48:32 +0900

    Add a code about using slice.is_empty()

--- src/main.rs
+++ src/main.rs
@@ -78,5 +78,6 @@ fn main() {
         assert_eq!(&full[2..4], "ok");
         assert_eq!(full[..].len(), 11);
         assert_eq!(full[5..].contains("boo"), false);
+        assert_eq!(full[0..0].is_empty(), true);
     }
 }
