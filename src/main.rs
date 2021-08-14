37626dce16ddbcc4b57f62650bf585b4a79f52f9
raimon <raimon49@hotmail.com>
2021-08-14 11:32:47 +0900

    Add a code about using string.find()

--- src/main.rs
+++ src/main.rs
@@ -140,5 +140,6 @@ fn main() {
     {
         let haystack = "One fine day, in the middle of the night";
         assert_eq!(haystack.find(','), Some(12));
+        assert_eq!(haystack.find("night"), Some(35));
     }
 }
