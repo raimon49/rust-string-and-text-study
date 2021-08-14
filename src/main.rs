29cae04b923f59b9801868b7f7d55d8bace2cb69
raimon <raimon49@hotmail.com>
2021-08-14 11:35:04 +0900

    Add a code about using string.find()

--- src/main.rs
+++ src/main.rs
@@ -141,5 +141,6 @@ fn main() {
         let haystack = "One fine day, in the middle of the night";
         assert_eq!(haystack.find(','), Some(12));
         assert_eq!(haystack.find("night"), Some(35));
+        assert_eq!(haystack.find(char::is_whitespace), Some(3)); // パターンとしてのFnMut(char) -> boolは、クロージャがtrueを返す1文字にマッチ
     }
 }
